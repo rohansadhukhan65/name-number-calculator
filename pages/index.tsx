cd041806cde1b52733a97cbe70251c10410df082
Rohan Sadhukhan <68492239+rohansadhukhan65@users.noreply.github.com>
2024-06-08 18:48:35 +0530

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,185 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useState } from "react";
 
 export default function Home() {
+  const [text, setText] = useState("");
+  const [calculation, setCalculation] = useState("");
+  const [result, setResult] = useState<number | null>();
+  const letterNumbers = [
+    {
+      num: 1,
+      letterSmall: "a",
+      letterCap: "A",
+    },
+    {
+      num: 1,
+      letterSmall: "i",
+      letterCap: "I",
+    },
+    {
+      num: 1,
+      letterSmall: "j",
+      letterCap: "J",
+    },
+    {
+      num: 1,
+      letterSmall: "q",
+      letterCap: "Q",
+    },
+    {
+      num: 1,
+      letterSmall: "y",
+      letterCap: "Y",
+    },
+    {
+      num: 2,
+      letterSmall: "b",
+      letterCap: "B",
+    },
+    {
+      num: 2,
+      letterSmall: "k",
+      letterCap: "K",
+    },
+    {
+      num: 2,
+      letterSmall: "r",
+      letterCap: "R",
+    },
+    {
+      num: 3,
+      letterSmall: "c",
+      letterCap: "C",
+    },
+    {
+      num: 3,
+      letterSmall: "g",
+      letterCap: "G",
+    },
+    {
+      num: 3,
+      letterSmall: "l",
+      letterCap: "L",
+    },
+    {
+      num: 3,
+      letterSmall: "s",
+      letterCap: "S",
+    },
+    {
+      num: 4,
+      letterSmall: "d",
+      letterCap: "D",
+    },
+    {
+      num: 4,
+      letterSmall: "m",
+      letterCap: "M",
+    },
+    {
+      num: 4,
+      letterSmall: "t",
+      letterCap: "T",
+    },
+    {
+      num: 5,
+      letterSmall: "e",
+      letterCap: "E",
+    },
+    {
+      num: 5,
+      letterSmall: "h",
+      letterCap: "H",
+    },
+    {
+      num: 5,
+      letterSmall: "n",
+      letterCap: "N",
+    },
+    {
+      num: 6,
+      letterSmall: "u",
+      letterCap: "U",
+    },
+    {
+      num: 6,
+      letterSmall: "v",
+      letterCap: "V",
+    },
+    {
+      num: 6,
+      letterSmall: "w",
+      letterCap: "W",
+    },
+    {
+      num: 6,
+      letterSmall: "x",
+      letterCap: "X",
+    },
+    {
+      num: 7,
+      letterSmall: "o",
+      letterCap: "O",
+    },
+    {
+      num: 7,
+      letterSmall: "z",
+      letterCap: "Z",
+    },
+    {
+      num: 8,
+      letterSmall: "f",
+      letterCap: "F",
+    },
+    {
+      num: 8,
+      letterSmall: "p",
+      letterCap: "P",
+    },
+  ];
+
+  const inputOnChangeHandler = (e: any) => {
+    const { value } = e.target;
+    const strArry = value.replaceAll(/\s+/g, "").split("");
+    const calculationArray = strArry.map((i: any) => {
+      const { num }: any = letterNumbers.find(
+        (v) => v.letterSmall === i || v.letterCap === i
+      );
+      return num;
+    });
+    let calculationStr: string = "";
+    calculationArray.map((v: any) => {
+      calculationStr += `${calculationStr !== "" ? "+" : ""}${v}`;
+    });
+    let result: number |null = null
+    calculationArray.map((v: any) => {
+      result += v;
+    });
+    setText(value);
+    setCalculation(calculationStr);
+    setResult(result)
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-[100dvh] w-full flex items-center justify-center">
+      <div className="bg-blue-800 w-[33rem] text-center text-white rounded-xl p-10 shadow-xl">
+        <h1 className="text-3xl text-center font-bold">
+          Name Number Calculator
+        </h1>
+        <div className="my-5 flex justify-center items-center w-full">
+          <input
+            onChange={inputOnChangeHandler}
+            type="text"
+            placeholder="Enter The Text"
+            className="p-3 rounded-xl shadow border-blue-900 w-full my-5 text-black outline-none"
+          />
+        </div>
+        <div className="my-5">
+          <p className="font-bold text-3xl my-5">{text}</p>
+          <p className="font-normal text-xl">{calculation}</p>
+          <p className="font-bold text-3xl my-5">{result}</p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
